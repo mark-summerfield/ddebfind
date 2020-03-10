2c402f751e121058f1ee3324a5ca78ad5d8bd437
mark-summerfield <mark@qtrac.eu>
2020-03-10 08:39:39 +0000

    eliminated false positive lint

--- st.sh
+++ st.sh
@@ -2,5 +2,6 @@ tokei -s lines -f -t=D -etests
 dscanner --styleCheck \
     | grep -v Public.declaration.*is.undocumented \
     | grep -v appwindow.d.*Variable.[ab].is.never.modified \
-    | grep -v model.d.*Variable.in[DC].*tion.is.never.modified
+    | grep -v model.d.*Variable.in[DC].*tion.is.never.modified \
+    | grep -v model_test.d.*Variable.timer.is.never.modified
 git status
